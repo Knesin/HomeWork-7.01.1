7a3cee661f98c694f6c4278dc7e160b7d5057d3d
Maksim Kl <GuruHacker@yandex.ru>
2023-09-01 10:49:45 +0300

    Initial commit

---
+++ sql_query_builder.cpp
@@ -0,0 +1,51 @@
+#include "sql_query_builder.h"
+
+sql_query_builder::sql_query_builder() {}
+
+sql_query_builder& sql_query_builder::AddColumn(const std::string& s) {
+	_collum.push_back(s);
+	return *this;
+};
+
+sql_query_builder& sql_query_builder::AddFrom(const std::string& s) {
+	_from = s;
+	return *this;
+};
+
+sql_query_builder& sql_query_builder::AddWhere(const std::string& collum, const std::string& value) {
+	_where[collum] = value;
+	return *this;
+};
+
+std::string sql_query_builder::BuildQuery() {
+	std::string result = "SELECT ";
+	if (_collum.empty()) {
+		result += "* ";
+	}
+	else {
+		for (const auto& s : _collum) {
+			result += s;
+			if (&s == &_collum.back()) {
+				result += " ";
+			}
+			else {
+				result += ", ";
+			}
+		}
+	}
+	result += "FROM " + _from;
+	if (!_where.empty()) {
+		result += " WHERE ";
+		for (const auto& m : _where) {
+			result += m.first + "=" + m.second;
+			if (&m.first != &_where.rbegin()->first) {
+				result += " AND ";
+			}
+		}
+	}
+	result += ";";
+	_collum.clear();
+	_from = "";
+	_where.clear();
+	return result;
+};  
